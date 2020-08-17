c5290393d65ec065a397a2c961b7aef812ad4944
elvis <elvis.pysqyli@gmail.com>
2020-08-17 16:07:55 +0200

    Functions changed for sequential steps

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,24 @@
-let selection = Array();
-let sSelection = String();
+let input = Array();
 
-function add(selection) {
-    return selection.reduce((a, b) => a+b);
+function add() {
+    return a + b;
 }
 
-function subtract(selection) {
-    return selection.reduce((a, b) => a-b);
+function subtract() {
+    return a - b;
 }
 
-function multiply(selection) {
-    return selection.reduce((a,b) => a*b);
+function multiply() {
+    return a * b;
 }
 
-function divide(selection) {
-    return selection.reduce((a,b) => a/b);
+function divide() {
+    return a / b;
 }
 
 function operate(operation, a, b) {
-    selection.push(a, b);
-    return operation(selection);
+    input.push(a, b);
+    return operation(input);
 }
 
 const display = document.querySelector("#result");
@@ -35,71 +34,76 @@ const nEight = document.getElementById("8");
 const nNine = document.getElementById("9");
 
 nZero.addEventListener('click', () => {
-    console.log(nZero.textContent);
-    selection.push(nZero.textContent);
-    sArray(); 
+    input.push(nZero.textContent);
+    createNum();
 })
 
 nOne.addEventListener('click', () => {
-    console.log(nOne.textContent);
-    selection.push(nOne.textContent);
-    sArray();
+    input.push(nOne.textContent);
+    createNum();
 })
 
 nTwo.addEventListener('click', () => {
-    console.log(nTwo.textContent);
-    selection.push(nTwo.textContent);
-    sArray();
+    input.push(nTwo.textContent);
+    createNum();
 })
 
 nThree.addEventListener('click', () => {
-    console.log(nThree.textContent);
-    selection.push(nThree.textContent);
-    sArray();
+    input.push(nThree.textContent);
+    createNum();
 })
 
 nFour.addEventListener('click', () => {
-    console.log(nFour.textContent);
-    selection.push(nFour.textContent);
-    sArray();
+    input.push(nFour.textContent);
+    createNum();
 })
 
 nFive.addEventListener('click', () => {
-    console.log(nFive.textContent);
-    selection.push(nFive.textContent);
-    sArray();
+    input.push(nFive.textContent);
+    createNum();
 })
 
 nSix.addEventListener('click', () => {
-    console.log(nSix.textContent);
-    selection.push(nSix.textContent);
-    sArray();
+    input.push(nSix.textContent);
+    createNum();
 })
 
 nSeven.addEventListener('click', () => {
-    console.log(nSeven.textContent);
-    selection.push(nSeven.textContent);
-    sArray();
+    input.push(nSeven.textContent);
+    createNum();
 })
 
 nEight.addEventListener('click', () => {
-    console.log(nEight.textContent);
-    selection.push(nEight.textContent);
-    sArray();
+    input.push(nEight.textContent);
+    createNum();
 })
 
 nNine.addEventListener('click', () => {
-    console.log(nNine.textContent);
-    selection.push(nNine.textContent);
-    sArray();
+    input.push(nNine.textContent);
+    createNum();
 })
 
-function sArray() {
-    sSelection = selection.join('');
-    dSelection = document.createElement('div');
-    dSelection.setAttribute('id', 'resultbox');
-    dSelection.textContent = sSelection;
+function createNum() {
+    sinput = input.join('');
+    dinput = document.createElement('div');
+    dinput.setAttribute('id', 'resultbox');
+    dinput.textContent = sinput;
     if (display.childElementCount > 0) display.removeChild(display.lastChild);
-    display.appendChild(dSelection);
-    return sSelection;
+    display.appendChild(dinput);
+    ninput = Number(sinput);
+    return sinput, ninput;
 }
+
+const addition = document.getElementById('add');
+addition.addEventListener('click', () => {
+    a = ninput;
+    input = [];
+    operation = add;
+    return a;
+})
+
+const enter = document.getElementById('enter');
+enter.addEventListener('click', () => {
+    b = ninput;
+    console.log(a + b);
+})
