19757a0c3f694da0deb7dda1f077f8c0856ccc04
epysqyli <elvis.pysqyli@gmail.com>
2020-08-24 16:07:45 +0200

    Improved calculate function

--- scripts/script.js
+++ scripts/script.js
@@ -104,51 +104,94 @@ function createNum() {
     return sinput, ninput;
 }
 
-// each function should solve one single problem
-
-function push() {
-    operations.forEach(function (operation) {
-        operation.addEventListener('click', () => {
-            console.log(operation.id);
-            inputList.push(ninput);
-            if (inputList.length >= 1) {
-                dinput.textContent = "";
-                while (input.length > 0) input.pop();
-            }
-        })
+//pushes input into inputList
+operations.forEach(function (operation) {
+    operation.addEventListener('click', () => {
+        console.log(operation.id);
+        inputList.push(ninput);
+        if (inputList.length >= 1) {
+            dinput.textContent = "";
+            while (input.length > 0) input.pop();
+        }
     })
-}
+})
 
-function chooser() {
-    operations.forEach(function (operation) {
-        operation.addEventListener('click', () => {
-            if (operation.id == "add") opOrder.push("add");
-            else if (operation.id == "subtract") opOrder.push("subtract");
-            else if (operation.id == "multiply") opOrder.push("multiply");
-            else if (operation.id == "divide") opOrder.push("divide");
-            else if (operation.id == 'enter') opOrder.push("enter");
-        })
+// saves the operation order and executes the calculation
+operations.forEach(function (operation) {
+    operation.addEventListener('click', () => {
+        if (operation.id == "add") {
+            opOrder.push("add");
+            console.log(opOrder);
+            console.log(inputList);
+            calculate();
+        }
+        else if (operation.id == "subtract") {
+            opOrder.push("subtract");
+            console.log(opOrder);
+            console.log(inputList);
+            calculate();
+        }
+        else if (operation.id == "multiply") {
+            opOrder.push("multiply");
+            console.log(opOrder);
+            console.log(inputList);
+            calculate();
+        }
+        else if (operation.id == "divide") {
+            opOrder.push("divide");
+            console.log(opOrder);
+            console.log(inputList);
+            calculate();
+        }
+        else if (operation.id == 'enter') {
+            opOrder.push("enter");
+            console.log(opOrder);
+            console.log(inputList);
+            calculate();
+            while (opOrder.length > 0) opOrder.pop();
+        }
     })
-}
+})
 
-function calculate(intRes) {
+
+function calculate() {
     if ((inputList.length == 2) && (opOrder[0] == 'add')) {
         intRes = operate(add, inputList[0], inputList[1]);
+        while (inputList.length > 0) inputList.pop();
+        inputList.push(intRes);
         opOrder.splice(0, 1);
+        console.log(intRes);
         return intRes;
     }
     if ((inputList.length == 2) && (opOrder[0] == 'subtract')) {
         intRes = operate(subtract, inputList[0], inputList[1]);
+        while (inputList.length > 0) inputList.pop();
+        inputList.push(intRes);
         opOrder.splice(0, 1);
+        console.log(intRes);
+        return intRes;
     }
     if ((inputList.length == 2) && (opOrder[0] == 'multiply')) {
         intRes = operate(multiply, inputList[0], inputList[1]);
+        while (inputList.length > 0) inputList.pop();
+        inputList.push(intRes);
         opOrder.splice(0, 1);
+        console.log(intRes);
+        return intRes;
     }
     if ((inputList.length == 2) && (opOrder[0] == 'divide')) {
-        intRes = operate(division, inputList[0], inputList[1]);
+        intRes = operate(divide, inputList[0], inputList[1]);
+        while (inputList.length > 0) inputList.pop();
+        inputList.push(intRes);
         opOrder.splice(0, 1);
+        console.log(intRes);
+        return intRes;
+    }
+    if ((inputList.length == 2) && (opOrder[0] == 'enter')) {
+        intRes = operate(opOrder[opOrder.length - 2], inputList[0], inputList[1]);
+        //while (inputList.length > 0) inputList.pop();
+        opOrder.splice(0, 1);
+        inputList.push(intRes);
+        return intRes;
     }
 }
-chooser();
-push();
